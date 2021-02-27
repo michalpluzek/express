12b79a19d0a40cfa136c83dfa34619de7499e627
Michał Płużek <michalpluzek@gmail.com>
2021-02-27 16:18:19 +0100

    admin login, admin news

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  db:
+    "mongodb+srv://admin:7lVSPnYf6CKBR7aZ@cluster0.5ynpb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  keySession: ["TWOJKLUCZ"],
+  maxAgeSession: 24 * 60 * 60 * 1000, // 24 hours
+};
