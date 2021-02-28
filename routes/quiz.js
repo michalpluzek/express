b08a249b74048b6805d4757344e97582805782f9
Michał Płużek <michalpluzek@gmail.com>
2021-02-28 18:24:16 +0100

    quiz form

--- routes/quiz.js
+++ routes/quiz.js
@@ -1,9 +1,33 @@
 const express = require("express");
 const router = express.Router();
+const Quiz = require("../models/quiz");
 
 /* GET home page. */
 router.get("/", (req, res) => {
-  res.render("quiz", { title: "Quiz" });
+  // new Quiz({ title: "Pytanie 1", vote: 0 }).save(); // tworzy i dodaje model w bazie
+  const show = !req.session.vote;
+
+  Quiz.find({}, (err, data) => {
+    // console.log(data);
+    let sum = 0;
+    data.forEach((item) => {
+      sum += item.vote;
+    });
+
+    res.render("quiz", { title: "Quiz", data, show, sum });
+  });
+});
+
+router.post("/", (req, res) => {
+  const id = req.body.quiz;
+
+  Quiz.findOne({ _id: id }, (err, data) => {
+    data.vote = data.vote + 1;
+    data.save(() => {
+      req.session.vote = 1;
+      res.redirect("/quiz"); //redirect wykona się dopiero gdy wykona się save
+    });
+  });
 });
 
 module.exports = router;
